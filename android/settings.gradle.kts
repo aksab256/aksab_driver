e2f1020d4085234641b765c0353d626357eee40e
aksab256 <amrshipl0@gmail.com>
2026-02-22 21:14:15 +0200

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -31,7 +31,7 @@ plugins {
     id("com.google.gms.google-services") version "4.4.1" apply false
     id("com.google.firebase.crashlytics") version "3.0.2" apply false
     
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
 include(":app")
